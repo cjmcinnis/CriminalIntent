85e650415e4538bee99ebd79dde8abdd4e225916
Craig <craigmcinnis2@gmail.com>
2018-08-26 09:49:21 -0300

    Added chapter 8 code

---
+++ app/src/main/java/com/cmcinnis/craig/criminalintent/Crime.java
@@ -0,0 +1,54 @@
+package com.cmcinnis.craig.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+    private boolean mRequirePolice;
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+        mRequirePolice = false; //default of false
+    }
+
+    public boolean isRequirePolice() {
+        return mRequirePolice;
+    }
+
+    public void setRequirePolice(boolean requiresPolice) {
+        mRequirePolice = requiresPolice;
+    }
+}
--- app/src/main/java/com/cmcinnis/craig/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/cmcinnis/craig/criminalintent/CrimeListActivity.java
@@ -2,10 +2,10 @@ package com.cmcinnis.craig.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFrameActivity {
+public class CrimeListActivity extends SingleFrameActivity{
 
     @Override
     protected Fragment createFragment(){
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
